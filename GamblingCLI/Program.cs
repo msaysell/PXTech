d35382bc3f985e54291ef3f1dcd15cbbf861fe31
Michael Saysell <mike@saysell.net>
2018-07-17 22:44:32 +0100

    Added limit checkss to console output

--- GamblingCLI/Program.cs
+++ GamblingCLI/Program.cs
@@ -7,7 +7,7 @@ namespace GamblingCLI
 {
     class Program
     {
-        static readonly int DEFAULT_SIMULATIONS = 10000;
+        static readonly int DEFAULT_SIMULATIONS = 100000;
         static void Main(string[] args)
         {
             int simulationCount = DEFAULT_SIMULATIONS;
@@ -25,6 +25,7 @@ namespace GamblingCLI
         static bool SimulateOutcome(int iterationCount)
         {
             var diceRoll = new DiceRollMachine();
+            diceRoll.DiceRoller.DiceFaces = 4;
             var roulette = new RouletteMachine();
             var rouletteResults = roulette.Simulate(iterationCount);
             var diceRollResults = diceRoll.Simulate(iterationCount);
@@ -35,9 +36,15 @@ namespace GamblingCLI
             var diceRollPercentage = OutputSimResults(diceRollTitle, iterationCount, diceRollResults);
             var bestMachine = "Neither";
             if (roulettePercentage > diceRollPercentage)
-                bestMachine = diceRollTitle;
-            else if (roulettePercentage < diceRollPercentage)
-                bestMachine = rouletteTitle;
+                if (IsWithinProfitLimit(rouletteTitle, roulettePercentage))
+                    bestMachine = rouletteTitle;
+                else
+                    bestMachine = diceRollTitle;
+            else if (diceRollPercentage > roulettePercentage)
+                if (IsWithinProfitLimit(diceRollTitle, diceRollPercentage))
+                    bestMachine = diceRollTitle;
+                else
+                    bestMachine = rouletteTitle;
 
             Console.WriteLine($"{bestMachine} would provide the best profit margin.\n");
 
@@ -47,13 +54,32 @@ namespace GamblingCLI
 
         }
 
+        static bool IsWithinProfitLimit(string machineName, float percentage)
+        {
+            const float profitLimit = 0.1f;
+            if (percentage > profitLimit)
+            {
+                Console.WriteLine($"ALERT: Profit Limit ({profitLimit:P2}) exceeded for {machineName}! Machine ineligible.");
+                return false;
+            }
+
+            if (percentage < 0)
+            {
+                Console.WriteLine($"ALERT: {machineName} did not make any profit! Machine ineligible.");
+                return false;
+            }
+
+            return true;
+
+        }
+
         static float OutputSimResults(string machineName, int simulationCount, BettingResult resultsAvg)
         {
             var totalBet = resultsAvg.InitialStake;
             var totalWinnings = resultsAvg.Returns;
-            var profitPercentage = totalWinnings / totalBet;
+            var profitPercentage = 1.0f - (totalWinnings / totalBet);
 
-            Console.WriteLine($"{machineName}: {simulationCount:n0} tests were run; Profit: £{totalBet - totalWinnings}; Profit Percentage: {1.0 - profitPercentage:P2}");
+            Console.WriteLine($"{machineName}: {simulationCount:n0} tests were run; Profit: £{totalBet - totalWinnings}; Profit Percentage: {profitPercentage:P2}");
 
             return profitPercentage;
 
